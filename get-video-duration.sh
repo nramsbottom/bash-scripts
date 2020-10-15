5c6d323b1de3189fce7a67b1c8557a590cabd2b7
Neil Ramsbottom <nramsbottom@users.noreply.github.com>
2020-10-15 22:04:48 +0200

    Adds a script that extracts duration from video file.

---
+++ get-video-duration.sh
@@ -0,0 +1,12 @@
+
+# This script extracts the length of a given media file and prints it in seconds.
+
+if [[ -f "$1" ]]; then
+	ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1 |  awk -F"." '{print $1}'
+	if [ ${PIPESTATUS[0]} -ne 0 ]; then
+		echo "ffmpeg call failed."
+		exit
+	fi
+else
+	echo "Input file does not exist."
+fi
